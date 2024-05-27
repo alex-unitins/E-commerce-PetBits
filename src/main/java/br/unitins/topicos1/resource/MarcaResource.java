5dc6f14dbc18cdd4594a6fd62f2304da2e587cb1
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-26 22:55:17 -0300

    caminhos para imagens de produtos e marca

--- src/main/java/br/unitins/topicos1/resource/MarcaResource.java
+++ src/main/java/br/unitins/topicos1/resource/MarcaResource.java
@@ -1,12 +1,17 @@
 package br.unitins.topicos1.resource;
 
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
 import br.unitins.topicos1.dto.MarcaDTO;
+import br.unitins.topicos1.form.ImageForm;
+import br.unitins.topicos1.service.MarcaFileServiceImpl;
 import br.unitins.topicos1.service.MarcaService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -14,6 +19,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.Response.Status;
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,6 +30,8 @@ public class MarcaResource {
     @Inject
     public MarcaService marcaService;
 
+    @Inject
+    public MarcaFileServiceImpl fileService;
     @GET
 
     @Path("/{id}")
@@ -66,6 +74,21 @@ public class MarcaResource {
         marcaService.delete(id);
         return Response.status(Status.NO_CONTENT).build();
     }
+    @PATCH
+    @Path("/{id}/image/upload")
+    @RolesAllowed("Admin")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@PathParam("id") Long id, @MultipartForm ImageForm form) {
+        fileService.salvar(id, form.getNomeImagem(), form.getImagem());
+        return Response.noContent().build();
+    }
 
+    @GET
+    @Path("/image/download/{nomeImagem}")
+    public Response download(@PathParam("nomeImagem") String nomeImagem) {
+        ResponseBuilder response = Response.ok(fileService.download(nomeImagem));
+        response.header("Content-Disposition", "attachment;filename=" + nomeImagem);
+        return response.build();
+    }   
 
 }
--- src/main/java/br/unitins/topicos1/resource/PetiscoResource.java
+++ src/main/java/br/unitins/topicos1/resource/PetiscoResource.java
@@ -1,12 +1,17 @@
 package br.unitins.topicos1.resource;
 
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
 import br.unitins.topicos1.dto.PetiscoDTO;
+import br.unitins.topicos1.form.ImageForm;
+import br.unitins.topicos1.service.PetiscoFileServiceImpl;
 import br.unitins.topicos1.service.PetiscoService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -14,6 +19,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.Response.Status;
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,6 +29,9 @@ public class PetiscoResource {
     
     @Inject
     public PetiscoService petiscoService;
+
+    @Inject
+    public PetiscoFileServiceImpl fileService;
 
     
 
@@ -89,5 +98,21 @@ public class PetiscoResource {
         petiscoService.delete(id);
         return Response.status(Status.NO_CONTENT).build();
     }
+    @PATCH
+    @Path("/{id}/image/upload")
+    @RolesAllowed("Admin")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@PathParam("id") Long id, @MultipartForm ImageForm form) {
+        fileService.salvar(id, form.getNomeImagem(), form.getImagem());
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Path("/image/download/{nomeImagem}")
+    public Response download(@PathParam("nomeImagem") String nomeImagem) {
+        ResponseBuilder response = Response.ok(fileService.download(nomeImagem));
+        response.header("Content-Disposition", "attachment;filename=" + nomeImagem);
+        return response.build();
+    }   
 
 }
