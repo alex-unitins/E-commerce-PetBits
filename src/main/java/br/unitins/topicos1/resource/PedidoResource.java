567ec671fd6957cd5055a6b2474d3f352c52c456
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-29 12:32:25 -0300

    pedido resource

--- src/main/java/br/unitins/topicos1/resource/PedidoResource.java
+++ src/main/java/br/unitins/topicos1/resource/PedidoResource.java
@@ -3,15 +3,23 @@ package br.unitins.topicos1.resource;
 import br.unitins.topicos1.dto.PedidoDTO;
 import br.unitins.topicos1.service.PedidoService;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("/pedidos")
 public class PedidoResource {
 
+    @Inject
     public PedidoService pedidoService;
 
     @GET
